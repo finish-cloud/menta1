6cd5bd4f003ad7652cfcbb51f0b59bc408932c1e
masaya <hope.is.a.waking.dream.8.1@gmail.com>
2021-04-22 20:58:34 +0900

    課題1-3終了しました

--- search.py
+++ search.py
@@ -1,24 +1,30 @@
 # 検索ツールサンプル
 # これをベースに課題の内容を追記してください
-
+import pandas as pd
 # 検索ソース
-source = ["ねずこ", "たんじろう", "きょうじゅろう", "ぎゆう", "げんや", "かなお", "ぜんいつ"]
 
 # 検索ツール
 
 
 def search():
-    word = input("鬼滅の登場人物の名前を入力してください >>> ")
 
-    # ここに検索ロジックを書く
-    if word in source:
-        print("{}が見つかりました".format(word))
-    else:
-        print("{}は見つかりませんでした".format(word))
-        # 追加
-        add_chara = input("追加登録しますか？(0:しない 1:する)　＞＞　")
-        if add_chara == "1":
-            source.append(word)
+    df = pd.read_csv("./Book1.csv")
+    source = list(df["name"])
+
+    while True:
+
+        word = input("鬼滅の登場人物の名前を入力してください >>> ")
+
+        # ここに検索ロジックを書く
+
+        if word in source:
+            print("{}が見つかりました".format(word))
+        else:
+            print("{}は見つかりませんでした".format(word))
+            # 追加
+            add_chara = input("追加登録しますか？(0:しない 1:する)　＞＞　")
+            if add_chara == "1":
+                source.append(word)
 
 
 if __name__ == "__main__":
